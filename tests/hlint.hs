277d8544ef2b9c5ebd631b6fbac5b4de996b2861
Andy Sonnenburg <andy22286@gmail.com>
2013-11-12 17:32:22 -0600

    boilerplate

--- tests/hlint.hs
+++ tests/hlint.hs
@@ -8,5 +8,5 @@ import System.Exit
 main :: IO ()
 main = do
   args <- getArgs
-  hints <- hlint $ ["src", "--cpp-define=HLINT", "--cpp-ansi"] ++ args
+  hints <- hlint $ ["src", "tools", "--cpp-define=HLINT", "--cpp-ansi"] ++ args
   unless (null hints) exitFailure
